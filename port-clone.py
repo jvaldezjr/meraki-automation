ffb0cd491bc28738b1a095254db649c326f9aa54
Jose Valdez <jose.valdez@meraki.net>
2021-02-09 06:53:39 -0600

    Basic working clone script logic with user input

--- port-clone.py
+++ port-clone.py
@@ -1,13 +1,78 @@
 import meraki
 import os
+import re
+import pprint
 
+# Configure your API key ENV variable using 'export MERAKI_DASHBOARD_API_KEY=YOUR_KEY'
 dashboard = meraki.DashboardAPI(os.environ['MERAKI_DASHBOARD_API_KEY'])
 
+# Get the list of organizations that the user has priveleges on
 orgs = dashboard.organizations.getOrganizations()
 
+# Print the list of organization names
 for i, org in enumerate(orgs):
     print (i, ":", org['name'])
 
-selected_org = input("Which organization would you like to select? ")
+# Prompt the user for which organization they'd like to configure
+selected_org = int(input("Which organization would you like to select? "))
 
-print(orgs[int(selected_org)]['id'])
+# Get the list of devices in the selected organization
+org_devices = dashboard.organizations.getOrganizationDevices(
+    orgs[selected_org]['id'], total_pages='all'
+)
+
+# Filter the list of devices for only switches
+def get_product(devices, type):
+    return list(filter(lambda device: re.match(type, device['model']), devices))
+
+org_switches = get_product(org_devices, "MS")
+
+# Print the list of switches in the organization
+def print_switches(switches):
+    for i, switch in enumerate(switches):
+        print (i, ":", switch['name'], "/", switch['serial'])
+
+# Get a port config and remove portId key / value pair
+def get_port_config(serial, port):
+    config = dashboard.switch.getDeviceSwitchPort(
+        serial, port
+    )
+    del config['portId']
+    return config
+
+print_switches(org_switches)
+
+# Prompt the user for the source switch and source port to be cloned
+source_switch = org_switches[int(input("Which source switch would you like to select? "))]
+source_port = int(input("Which source port would you like to clone? "))
+
+# Store port config to be cloned
+port_config = get_port_config(source_switch['serial'], source_port)
+
+print_switches(org_switches)
+
+# Get the destination switch and port to be configured
+destination_switch = org_switches[int(input("Which destination switch would you like to configure? "))]
+destination_port = int(input("Which destination port would you like to configure? "))
+
+# Confirm the config about to be cloned
+print(f"The following source port config will be cloned from {source_switch['name']} to {destination_switch['name']} port {destination_port}: ")
+pprint.pprint(port_config)
+
+# Get user consent to proceed
+while True:
+  try:
+    proceed = input("Carry on? (y/n): ")
+    if proceed.lower() == 'n':
+        print("Quitting program...")
+        quit()
+    elif proceed.lower() == 'y':
+      break
+    print("Invalid response entered, please respond with y or n")
+  except Exception as e:
+    print(e)
+
+# Configure destination port
+dashboard.switch.updateDeviceSwitchPort(
+    switches[destination_switch]['serial'], destination_port, **port_config
+)
